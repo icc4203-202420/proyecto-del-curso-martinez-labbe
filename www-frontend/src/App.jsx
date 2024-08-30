4922e6e9b8245aae7f7186904c94ff9f72402130
Labbe1234 <tlabbe@miuandes.cl>
2024-08-29 23:39:46 -0400

    Navbar y Home_Page

--- www-frontend/src/App.jsx
+++ www-frontend/src/App.jsx
@@ -1,17 +1,42 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-import { AppBar, Toolbar, Typography, IconButton, Drawer,Button,styled, List, ListItem, ListItemText, ListItemIcon, Card, CardHeader, CardContent } from '@mui/material';
-// import './App.css';
-import Beer from './assets/beer.svg';
-import Log_bar from './assets/login_bar.svg';
-
+import React, { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material';
+import { Link, Routes, Route } from 'react-router-dom';
+import CottageIcon from '@mui/icons-material/Cottage';
 import Home from './components/Home';
+import Beer from './assets/beer.svg';
+import './App.css';
 
 function App() {
-  return(<Routes>
-    <Route path="/" element={<Home />} />
-    </Routes>
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const toggleDrawer = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  return (
+    <>
+      <AppBar position="fixed" className="appbar">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" onClick={toggleDrawer}>
+            <img src={Beer} alt="beer-icon" className="beer-icon" />
+          </IconButton>
+          
+          <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer}>
+            <List>
+              <ListItem button component={Link} to="/">
+                <ListItemIcon>
+                  <CottageIcon />
+                </ListItemIcon>
+                <ListItemText primary="Home" />
+              </ListItem>
+            </List>
+          </Drawer>
+        </Toolbar>
+      </AppBar>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
--- www-frontend/src/components/Home.jsx
+++ www-frontend/src/components/Home.jsx
@@ -1,17 +1,85 @@
 import React from 'react';
-import { Button, Card, CardHeader } from '@mui/material';
-import Beer from '../assets/beer.svg';
+import { AppBar, Toolbar, Typography, IconButton, TextField, Tabs, Tab, Card, CardMedia, CardContent } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import './HomeBares.css'; 
 
-function Home() {
+function HomeBares() {
   return (
-    <div className="container">
-      <img src={Beer} alt="beer" className='beer-icon' />
-      <Card className='log_in'>
-        <CardHeader title="Welcome to Beer" className='header' />
-        <Button variant="contained" className='log_in_button'>Log In</Button>
-      </Card>
+    <div className="home-bares-container">
+      {/* AppBar */}
+      {/* <AppBar position="sticky" className="app-bar">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className="title">
+            Beer
+          </Typography>
+        </Toolbar>
+      </AppBar> */}
+
+      {/* Header */}
+      <div className="header">
+        <Typography variant="h5" className="question">
+        ¿Qué hay para <span className="highlight">beber</span> hoy?
+        </Typography>
+        <div className="search-bar">
+          <TextField 
+            fullWidth 
+            variant="outlined" 
+            placeholder="Search..." 
+            InputProps={{
+              startAdornment: <SearchIcon />
+            }}
+            className="search-input"
+          />
+        </div>
+      </div>
+
+      {/* Tabs */}
+      <div className="tabs-container">
+        <Tabs value={0} aria-label="bares y cervezas" className="tabs">
+          <Tab label="BARES" className="tab active" />
+          <Tab label="CERVEZAS" className="tab" />
+        </Tabs>
+      </div>
+
+      {/* List of Bars */}
+      <div className="bar-list">
+        {/* Bar Card */}
+        <Card className="bar-card">
+          <CardMedia
+            // component="img"
+            alt="Bar La Virgen"
+            height="140"
+          />
+          <CardContent>
+            <Typography variant="h6">BAR LA VIRGEN</Typography>
+            <Typography variant="body2" color="textSecondary">Chile</Typography>
+            <Typography variant="body2" color="textSecondary">Av. Vitacura 9191, Vitacura</Typography>
+            {/* <LocationOnIcon className="location-icon" /> */}
+          </CardContent>
+        </Card>
+
+        <Card className="bar-card">
+          <CardMedia
+            // component="img"
+            alt="Bar Teclados"
+            height="140"
+            // image="/assets/bar_teclados.jpg"
+          />
+          <CardContent>
+            <Typography variant="h6">BAR TECLADOS</Typography>
+            <Typography variant="body2" color="textSecondary">Chile</Typography>
+            <Typography variant="body2" color="textSecondary">Av. Vitacura 7235, Vitacura, Región Metropolitana</Typography>
+            {/* <LocationOnIcon className="location-icon" /> */}
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
 
-export default Home;
+export default HomeBares;
